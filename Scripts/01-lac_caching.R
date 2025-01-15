fd01f965779ff64df3e828c2e8ffd8695ae89ea5
Kaitlyn Courchesne <146389104+courchesnek@users.noreply.github.com>
2025-01-15 12:54:35 -0700

    models and plots

--- Scripts/01-lac_caching.R
+++ Scripts/01-lac_caching.R
@@ -42,17 +42,54 @@ female_cones <- females %>%
     by = c("squirrel_id", "year")) %>%
   filter(!is.na(log_cache_size_new) & !is.na(log_total_cones))
 
+#add a cache rate column - to control for cone crop
+female_cones <- female_cones %>%
+  mutate(cache_rate = log_cache_size_new / log_total_cones)
 
 
+# model -------------------------------------------------------------------
+##filter for positive caching events only (i.e. only want cache size new > 0)
+positive_caches <- female_cones %>%
+  filter(log_cache_size_new > 0)
+
+positive_caches$lac_cache <- as.logical(positive_caches$lac_cache)
+
+#fit the continuous model
+model_positive <- lm(log_cache_size_new ~ lac_cache + log_total_cones, data = positive_caches)
+
+#model summary
+summary(model_positive)
+
+#check residuals
+par(mfrow = c(2, 2))
+plot(model_positive)
 
 
+# plots -------------------------------------------------------------------
+#effect plot
+effect_plot(model_positive, pred = lac_cache,
+            data = positive_caches,
+            interval = TRUE, plot.points = TRUE,
+            x.label = "Total Cones (Log)",
+            y.label = "Predicted Log Cache Size",
+            main.title = "Effect of Total Cones and Lactation Status on Cache Size")
 
+#generate predictions
+positive_caches$predicted_cache_size <- predict(model_positive, newdata = positive_caches, type = "response")
 
-
-
-
-
-
+#violin plot
+ggplot(positive_caches, aes(x = lac_cache, y = cache_rate, fill = lac_cache)) +
+  geom_violin(trim = FALSE, color = "black") +
+  geom_boxplot(width = 0.1, fill = "white") +
+  geom_jitter(shape = 16, position = position_jitter(0.2), alpha = 0.5) +
+  labs(title = "Cache Rate by Lactation Status During Caching Season",
+       x = "Lactation Status During Caching Season",
+       y = "Cache Rate (Log New Cones Cached per Log Total Cones)") +
+  scale_fill_manual(values = c("FALSE" = "#A6CEE3", "TRUE" = "#FDBF6F")) +
+  scale_x_discrete(labels = c("FALSE" = "Non-Lactating", "TRUE" = "Lactating")) +
+  theme_minimal() +
+  theme(legend.position = "none") +
+  theme(plot.title = element_text(hjust = 0.5))
 
 
 
--- Scripts/03-males_females_nolaccache.R
+++ Scripts/03-males_females_nolaccache.R
@@ -20,3 +20,55 @@ f_nolaccache <- females %>%
 midden_cones_filtered <- midden_cones %>%
   filter(sex == "M" | squirrel_id %in% f_nolaccache$squirrel_id) %>%
   dplyr::select(year, grid, squirrel_id, sex, log_cache_size_new, log_total_cones)
+
+#add a cache rate column - to control for cone crop
+midden_cones_filtered <- midden_cones_filtered %>%
+  mutate(cache_rate = log_cache_size_new / log_total_cones)
+
+# model -------------------------------------------------------------------
+##filter for positive caching events only (i.e. only want cache size new > 0)
+positive_caches <- midden_cones_filtered %>%
+  filter(log_cache_size_new > 0)
+
+model_positive <- lm(log_cache_size_new ~ sex + log_total_cones, data = positive_caches)
+
+#model summary
+summary(model_positive)
+
+#check residuals
+par(mfrow = c(2, 2))
+plot(model_positive)
+
+
+# plot --------------------------------------------------------------------
+#violin plot
+ggplot(positive_caches, aes(x = sex, y = cache_rate, fill = sex)) +
+  geom_violin(trim = FALSE) +
+  geom_boxplot(width = 0.1, outlier.shape = NA, alpha = 0.7) +
+  geom_jitter(width = 0.2, size = 1, alpha = 0.5) +
+  labs(title = "Caching Rate by Sex",
+       x = "Sex",
+       y = "Cache Rate (Log New Cones Cached per Log Total Cones)") +
+  theme_minimal() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_fill_manual(values = c("M" = "#F8766D", "F" = "#00BFC4"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
